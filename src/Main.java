93bc9df4df817dc403abf00f2cc7e679945014e6
wilso3rc <wilso3rc@mail.uc.edu>
2023-06-06 20:59:58 -0400

    final with variables and output

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int springMaintenanceCost = 125;
+        int fallMaintenanceCost = 343;
+        int summerMaintenanceCost = 231;
+        int winterMaintenanceCost = 239;
+        int yearlyMaintenanceCost = 0;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        yearlyMaintenanceCost = springMaintenanceCost + fallMaintenanceCost + winterMaintenanceCost + summerMaintenanceCost;
+        System.out.println("Based on the seasonal maintenance costs of $" + springMaintenanceCost + ", $" + summerMaintenanceCost + ", $" + winterMaintenanceCost + ", and $" + fallMaintenanceCost + ". The yearly maintenance cost is: $" + yearlyMaintenanceCost);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
